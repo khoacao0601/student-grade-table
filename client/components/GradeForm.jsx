b59a49ce1723cfbef6abb68675d3fd716259241a
khoacao0601 <khoacao0601@gmail.com>
2020-01-20 23:37:32 -0800

    Update button

--- client/components/GradeForm.jsx
+++ client/components/GradeForm.jsx
@@ -42,13 +42,15 @@ export default class GradeFrom extends React.Component {
     return (
       <div className="grade-form" >
         <form onSubmit={this.sendValue} onReset={this.handleReset}>
-          <i className="fas fa-user"></i>
-          <input placeholder="Name" type="text" value={this.state.name} onChange={this.handleChangeName}></input>
+          <div className="">
+            <i className="fas fa-user"></i>
+            <input placeholder="Name" type="text" value={this.state.name} onChange={this.handleChangeName}></input>
+          </div>
           <i className="fas fa-book"></i>
           <input placeholder="Course" type="text" value={this.state.course} onChange={this.handleChangeCourse}></input>
           <i className="fas fa-graduation-cap"></i>
           <input placeholder="Grade" type="number" value={this.state.grade} onChange={this.handleChangeGrade}></input>
-          <div className="button-grade-from">
+          <div className="button-grade-from d-flex justify-content-left">
             <button type="submit" className="btn btn-success">Add</button>
             <button type="reset" className="btn btn-success">Cancel</button>
           </div>
