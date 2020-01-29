4369147d30f521582eb9aaf792414cae7fcb2a2d
khoacao0601 <khoacao0601@gmail.com>
2020-01-28 22:50:41 -0800

    Add update button

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -11,7 +11,10 @@ export default function Grade(props) {
       <td>{props.sending.name}</td>
       <td>{props.sending.course}</td>
       <td>{props.sending.grade}</td>
-      <td><button className="btn btn-danger center-block" onClick={deleteOne}>Delete</button></td>
+      <td>
+        <button className="btn btn-danger center-block mr-3" onClick={deleteOne}>Delete</button>
+        <button className="btn btn-warning center-block" onClick={deleteOne}>Update</button>
+      </td>
     </tr>
   );
 }
