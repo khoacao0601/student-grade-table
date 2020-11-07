2b1a4c4797f100f5decf1372d618e9837846965f
Khoa Cao <khoacao0601@gmail.com>
2020-11-06 23:17:19 -0800

    Update gradetable.jsx

--- client/components/gradetable.jsx
+++ client/components/gradetable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Grade from './grade';
+
 
 export default function GradeTable(props) {
   const table = props.table.map((object, indexArray) => <Grade key={indexArray} sending={object} deleteStudent={props.remove} update={props.update}/>);
@@ -12,7 +12,7 @@ export default function GradeTable(props) {
             <th scope="col" className="col-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">Student Name</th>
             <th scope="col" className="col-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">Course</th>
             <th scope="col" className="col-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">Grade</th>
-            <th scope="col" className="col-3 col-xl-5 col-lg-3 col-md-3 col-sm-3">Operation</th>
+            <th scope="col" className="col-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">Operation</th>
           </tr>
         </thead>
         <tbody>{table}</tbody>
